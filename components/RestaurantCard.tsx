bba7b44343ae22ac9a2c4a682a7324f6aafcf611
Jonathan Korol <korol.jonathan@gmail.com>
2023-01-03 19:09:21 +0100

    Amélioration génération affichage note

--- components/RestaurantCard.tsx
+++ components/RestaurantCard.tsx
@@ -10,68 +10,22 @@ restaurant : Restaurant
 export default function RestaurantCard(props : restaurantCardProps)
 {
     const [restaurant,setRestaurants] = useState<Restaurant>(props.restaurant);
-    const renderNote = ()=>{
-        return (
-            <View>
-                {restaurant.note==0?(
-                    <View style={{flexDirection: "row"}}>
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                    </View>
-                ):restaurant.note<=1.5?(
-                    <View style={{flexDirection: "row"}}>
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                    </View>
-                ):restaurant.note<=2.5?(
-                    <View style={{flexDirection: "row"}}>
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                    </View>
-                ):restaurant.note<=3.5?(
-                    <View style={{flexDirection: "row"}}>
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                    </View>
-                ):restaurant.note<=4.5?(
-                    <View style={{flexDirection: "row"}}>
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star-o" />
-                    </View>
-                ):(
-                    <View style={{flex : 1,flexDirection: "row"}}>
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                        <FontAwesome size={15} style={{  }} color="white" name="star" />
-                    </View>
-                )}
-            </View>
-        )
-    }
+    
     return(
         <View>
             <View>
             <Text>{restaurant.nom}</Text>
             </View>
             
-            <View>{renderNote()}</View>
+            <View>
+            <View style={{flexDirection: "row"}}>
+                <FontAwesome size={15} style={{  }} color="white" name={restaurant.note==0?"star-o":"star"} />
+                <FontAwesome size={15} style={{  }} color="white" name={restaurant.note<1.5?"star-o":"star"} />
+                <FontAwesome size={15} style={{  }} color="white" name={restaurant.note<2.5?"star-o":"star"} />
+                <FontAwesome size={15} style={{  }} color="white" name={restaurant.note<3.5?"star-o":"star"} />
+                <FontAwesome size={15} style={{  }} color="white" name={restaurant.note<4.5?"star-o":"star"} />
+            </View>
+            </View>
         </View>
     );
 }
