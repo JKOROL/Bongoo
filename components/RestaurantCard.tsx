249460fc83e34083d4faf0f63cc7c3c91e3894e2
Jonathan Korol <korol.jonathan@gmail.com>
2023-01-03 19:34:55 +0100

    Ajout d'image et de style sur les card restaurant

--- components/RestaurantCard.tsx
+++ components/RestaurantCard.tsx
@@ -1,4 +1,5 @@
 import { FontAwesome } from "@expo/vector-icons";
+import { StyleSheet, Image } from 'react-native';
 import React, { useState } from "react";
 import { Restaurant } from "../classes";
 import { View, Text } from "./Themed";
@@ -12,20 +13,41 @@ export default function RestaurantCard(props : restaurantCardProps)
     const [restaurant,setRestaurants] = useState<Restaurant>(props.restaurant);
     
     return(
-        <View>
-            <View>
-            <Text>{restaurant.nom}</Text>
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={{uri: restaurant.photo}}></Image>
             </View>
-            
-            <View>
-            <View style={{flexDirection: "row"}}>
-                <FontAwesome size={15} style={{  }} color="white" name={restaurant.note==0?"star-o":"star"} />
-                <FontAwesome size={15} style={{  }} color="white" name={restaurant.note<1.5?"star-o":"star"} />
-                <FontAwesome size={15} style={{  }} color="white" name={restaurant.note<2.5?"star-o":"star"} />
-                <FontAwesome size={15} style={{  }} color="white" name={restaurant.note<3.5?"star-o":"star"} />
-                <FontAwesome size={15} style={{  }} color="white" name={restaurant.note<4.5?"star-o":"star"} />
-            </View>
+            <View >
+                <View><Text>{restaurant.nom}</Text></View>
+                <View style={{flexDirection: "row"}}>
+                    <FontAwesome size={15} style={{  }} color="white" name={restaurant.note==0?"star-o":"star"} />
+                    <FontAwesome size={15} style={{  }} color="white" name={restaurant.note<1.5?"star-o":"star"} />
+                    <FontAwesome size={15} style={{  }} color="white" name={restaurant.note<2.5?"star-o":"star"} />
+                    <FontAwesome size={15} style={{  }} color="white" name={restaurant.note<3.5?"star-o":"star"} />
+                    <FontAwesome size={15} style={{  }} color="white" name={restaurant.note<4.5?"star-o":"star"} />
+                </View>
             </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        borderStyle: "solid",
+        borderColor: "white",
+        borderWidth:1,
+        borderRadius: 10,
+        margin: 2,
+        padding: 2
+    },
+    imageContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    image:{
+        borderRadius: 5,
+        width:  40,
+        height: 40
+    }
+});
