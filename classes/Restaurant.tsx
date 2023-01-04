722e96b04d1bbe8c62e56b5a82e25fbe4571a93e
Jonathan Korol <korol.jonathan@gmail.com>
2023-01-04 10:53:49 +0100

    Ajout de la position d'un restaurant et distance

--- classes/Restaurant.tsx
+++ classes/Restaurant.tsx
@@ -1,13 +1,19 @@
+import { getDistance } from "geolib";
+
 export default class Restaurant{
-    nom;
-    note;
-    distance;
-    photo;
-    constructor(nom:string)
+    nom : string;
+    note : number;
+    distance : number;
+    photo : string;
+    latitude : number;
+    longitude : number;
+    constructor(nom:string, userPosition:{latitude:number,longitude:number})
     {
         this.nom=nom;
+        this.latitude=47.850812;
+        this.longitude=1.892373;
         this.note=Math.floor(Math.random() * 5);
-        this.distance=Math.floor(Math.random() * 5);
+        this.distance=getDistance({latitude:this.latitude,longitude:this.longitude},userPosition)
         this.photo="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/273px-McDonald%27s_Golden_Arches.svg.png";
     }
 }
